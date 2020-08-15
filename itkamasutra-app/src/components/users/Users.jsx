6ee1456a6018601740bd0ec3eaf51a8ebb079aac
dmitry-mishchuk <Mishchukltd@gmail.com>
2020-08-15 23:48:00 +0300

    Lessons51 (Перед установкой axios)

--- itkamasutra-app/src/components/users/Users.jsx
+++ itkamasutra-app/src/components/users/Users.jsx
@@ -2,7 +2,24 @@ import React from 'react';
 import style from './user.module.css';
 
 const Users = (props) => {
-  debugger;
+
+debugger;
+  if(props.users.length === 0){
+    props.setUsers( [
+      {id: 1,
+        photoUrl:'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Dmitry_Nagiev_2017_4.jpg/274px-Dmitry_Nagiev_2017_4.jpg',
+        followed: false, firstName: 'Dima', status: 'I am a boss', location: {city:'Kyiv', contry:'Ukraine'}},
+      {id: 2,
+        photoUrl:'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Dmitry_Nagiev_2017_4.jpg/274px-Dmitry_Nagiev_2017_4.jpg',
+        followed: true, firstName: 'Olga', status: 'I am a teacher', location: {city:'Moscow', contry:'Russia'}},
+      {id: 3,
+        photoUrl:'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Dmitry_Nagiev_2017_4.jpg/274px-Dmitry_Nagiev_2017_4.jpg',
+        followed: false, firstName: 'Maks', status: 'The boy', location: {city:'Minsk', contry:'Belarus'}}
+    ] );
+  }
+
+
+
   return (
     <div>
     {
@@ -13,6 +30,7 @@ const Users = (props) => {
               <img src={user.photoUrl} className={style.photo}/>
             </div>
             <div>
+
               { user.followed ?
                 <button onClick={ () => { props.unfollow(user.id) } }>Unfollow</button> :
                 <button onClick={ () => { props.follow(user.id) } }>Follow</button> }
--- itkamasutra-app/src/redux/usersPageReduser.js
+++ itkamasutra-app/src/redux/usersPageReduser.js
@@ -3,21 +3,11 @@ const UNFOLLOW = "UNFOLLOW";
 const SET_USERS = "SET_USERS";
 
 let initialState = {
-  users: [
-    {id: 1,
-      photoUrl:'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Dmitry_Nagiev_2017_4.jpg/274px-Dmitry_Nagiev_2017_4.jpg',
-      followed: false, firstName: 'Dima', status: 'I am a boss', location: {city:'Kyiv', contry:'Ukraine'}},
-    {id: 2,
-      photoUrl:'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Dmitry_Nagiev_2017_4.jpg/274px-Dmitry_Nagiev_2017_4.jpg',
-      followed: true, firstName: 'Olga', status: 'I am a teacher', location: {city:'Moscow', contry:'Russia'}},
-    {id: 3,
-      photoUrl:'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Dmitry_Nagiev_2017_4.jpg/274px-Dmitry_Nagiev_2017_4.jpg',
-      followed: false, firstName: 'Maks', status: 'The boy', location: {city:'Minsk', contry:'Belarus'}}
-  ]
+  users: [  ]
 };
 
 const usersPageReduser = (state = initialState, action) => {
-
+debugger;
   if (action.type === FOLLOW) {
     return {
       ...state,
@@ -39,7 +29,8 @@ const usersPageReduser = (state = initialState, action) => {
       })
     };
   } else if (action.type === SET_USERS) {
-    return { ...state, users: [...state.users], ...action.users}
+    console.log(action.users);
+    return { ...state, users: [...state.users, ...action.users]}
   }
   return state;
 
